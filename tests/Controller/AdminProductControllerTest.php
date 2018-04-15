029df9b6ccdd161743876cb597e98d4576d37982
Artem <artem0100@ukr.net>
2018-04-15 22:33:04 +0300

    correct working PHPUnit (Functional tests)

--- tests/Controller/AdminProductControllerTest.php
+++ tests/Controller/AdminProductControllerTest.php
@@ -11,16 +11,16 @@ class AdminProductController extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/admin/new-product', [], [], [
-            "Authorization" => "Basic YWRtaW46YWRtaW4="
+            "HTTP_AUTHORIZATION" => "Basic YWRtaW46YWRtaW4="
         ]);
 
-        $form = $crawler->filter('#form_save')->form();
+        $form = $crawler->selectButton('Create product')->form();
 
-        $form['title'] = 'Name of product';
-        $form['price'] = 99.99;
-        $form['description'] = "Description about product";
+        $form['form[title]'] = 'Name of product';
+        $form['form[price]'] = 99.99;
+        $form['form[description]'] = "Description about product";
 
-        $crawler = $client->submit($form);
+        $client->submit($form);
 
         $this->assertTrue($client->getResponse()->isRedirect());
     }
